eeb36bbb4113595c611a9b96d945d0de15164548
Semen Kuzmin <eurosemen@yandex.ru>
2022-02-04 15:00:53 +0300

    добавил приложение для расчета бонусных миль

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 999;
+        int amountPrice = 20;
+        int bonusMili = priceTicket / amountPrice;
+        System.out.println(bonusMili);
+    }
+}
